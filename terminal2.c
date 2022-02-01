e7bc44f523dcbd3ae80aca1e89ab0381fff9550f
erlonbie <50176483+erlonbie@users.noreply.github.com>
2022-01-31 21:22:35 -0400

    pwd, ls (cd not working)

--- terminal2.c
+++ terminal2.c
@@ -3,30 +3,48 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
-#include "meu_ls.h"
-#include "meu_cd.h"
-#include "meu_exec.h"
 
-void prompt()
+char **parser(char *input)
 {
-    printf(">> ");
-}
-
-char **parser(char *frase)
-{
-    char **vetor = (char **)malloc(sizeof(char *) * strlen(frase));
+    char **vetor = (char **)malloc(sizeof(char *) * strlen(input));
     char *token, *parou;
     int i = 0;
 
-    parou = strdup(frase);
+    parou = strdup(input);
 
     while ((token = strsep(&parou, " ")) != NULL)
     {
         vetor[i] = token;
+        printf("%s\n", vetor[i]);
         i++;
     }
 
     return vetor;
+}
+
+void cmd_exit () {
+    int pid = getpid();
+    char str[5] = {0};
+    char * args[3];
+    sprintf(str, "%d", pid);
+    args[0] = strdup("kill");
+    args[1] = strdup(str);
+    args[2] = NULL;
+    execvp((args[0]), args);
+}
+
+void cmd_execute (char ** input) {
+    int rc = fork();
+    if (rc < 0) {
+        fprintf(stderr, "fork falhou\n");
+        exit(1);
+    } else if (rc == 0) {
+        execvp(input[0], input);
+        cmd_exit(); 
+    }
+    else {
+        int cmd = wait(NULL);
+    }
 }
 
 int main(int argc, char *argv[])
@@ -34,61 +52,28 @@ int main(int argc, char *argv[])
     system("clear");
     int tam;
     char **args;
+    char * input;
     while (1)
     {
-        //prompt();
         char *path = getcwd(NULL, 0);
         printf("%s >> ", path);
 
-        char frase[100];
-        if (argc > 1)
+        /* char input[100]; */
+        input = (char *) malloc(sizeof(char));
+        scanf("%[^\n]", input);
+        getchar();
+        args = parser(input);
+
+        if (strcmp(input, "exit") == 0)
         {
-            args = argv;
+            cmd_exit(); 
         }
-        else
-        {
-            scanf("%[^\n]", frase);
-            getchar();
-            args = parser(frase);
+        else {
+            cmd_execute(args);
         }
 
-        for (int j = 0; args[j] != NULL; j++)
-        {
-            tam = j;
-            //printf("Token %d: %s\n", j, args[j]);
-        }
-        tam++;
-        if (tam == 1)
-        {
-            args[1] = "";
-        }
-        //printf("tamanho: %d\n", tam);
-        if (strcmp(frase, "exit") == 0)
-        {
-            int pid = getpid();
-            char str[5] = {0};
-            sprintf(str, "%d", pid);
-            args[0] = strdup("kill");
-            args[1] = strdup(str);
-            args[2] = NULL;
-            execvp((args[0]), args);
-        }
-        else if (strcmp(args[0], "ls") == 0)
-        {
-            mostraArquivos(path);
-        }
-        else if (strcmp(args[0], "pwd") == 0)
-        {
-            printf("%s\n", path);
-        }
-        else if (strcmp(args[0], "cd") == 0)
-        {
-            mudaDiretorio(args[1]);
-        }
-        else
-        {
-            executaComando(args, tam);
-        }
+        free(input);
+        free(args);
     }
 
     return 0;
